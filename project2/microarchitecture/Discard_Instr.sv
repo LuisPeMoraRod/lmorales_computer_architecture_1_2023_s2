a1bcb511c472b02f573b967087c40254c7281aa8
LuisPeMoraRod <luismorarod98@estudiantec.cr>
2023-10-01 17:15:09 -0600

    feat(microarchitecture): add MIPS based pipeline control unit

---
+++ project2/microarchitecture/Discard_Instr.sv
@@ -0,0 +1,8 @@
+`timescale 1 ps / 100 fs
+// Discard instructions when needed
+module Discard_Instr(ID_flush,IF_flush,jump,bne,jr);
+	output ID_flush,IF_flush;
+	input jump,bne,jr;
+	or #50 OR1(IF_flush,jump,bne,jr);
+	or #50 OR2(ID_flush,bne,jr);
+endmodule
