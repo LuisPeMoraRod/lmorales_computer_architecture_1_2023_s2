2431c7608df9594fb2651118b8a7541618237a8b
LuisPeMoraRod <luismorarod98@estudiantec.cr>
2023-10-10 01:50:54 -0600

    fix: assign temp logic to output signal in mux3x32to32.sv

--- project2/microarchitecture/mux3x32to32.sv
+++ project2/microarchitecture/mux3x32to32.sv
@@ -4,13 +4,17 @@ module mux3x32to32(DataOut,A,B,C,Select);
 	input [1:0] Select;
 	input [31:0] A,B,C;
 
+	logic [31:0] DataOut_wire;
+
 	always @(*) begin
 		 case (Select)
-			2'b00: DataOut = A;
-			2'b01: DataOut = B;
-			2'b10: DataOut = C;
-			default: DataOut = 32'b0;
+			2'b00: DataOut_wire = A;
+			2'b01: DataOut_wire = B;
+			2'b10: DataOut_wire = C;
+			default: DataOut_wire = 32'b0;
 		 endcase
 	  end
+	
+	assign DataOut = DataOut_wire;
 
 endmodule
