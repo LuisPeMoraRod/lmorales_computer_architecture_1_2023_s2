c16aa327bb114361cc966e77a3203b2c900dbfc8
LuisPeMoraRod <luismorarod98@estudiantec.cr>
2023-10-17 13:56:51 -0600

    refactor(register file): update amount of registers and their size

--- project2/microarchitecture/regfile.sv
+++ project2/microarchitecture/regfile.sv
@@ -1,53 +1,35 @@
 `timescale 1 ps / 100 fs
 module regfile
  (
-		output [31:0] reg_read_data_1,
-		output [31:0] reg_read_data_2,
-		input [31:0] reg_write_data,
-		input [4:0] reg_read_addr_1,
-		input [4:0] reg_read_addr_2,
-		input [4:0] reg_write_dest,
+		output [23:0] reg_read_data_1,
+		output [23:0] reg_read_data_2,
+		input [23:0] reg_write_data,
+		input [3:0] reg_read_addr_1,
+		input [3:0] reg_read_addr_2,
+		input [3:0] reg_write_dest,
 		input reg_write_en,
 		input rst,
 		input clk
  );
-      reg     [31:0]     reg_array [31:0];  
-      // write port  
-      //reg [2:0] i;  
+      reg     [23:0]     reg_array [15:0];  
       always @ (posedge clk or posedge rst) begin  
            if(rst) begin  
-                reg_array[0] <= 32'b0;  
-                reg_array[1] <= 32'b0;  
-                reg_array[2] <= 32'b0;  
-                reg_array[3] <= 32'b0;  
-                reg_array[4] <= 32'b0;  
-                reg_array[5] <= 32'b0;  
-                reg_array[6] <= 32'b0;  
-                reg_array[7] <= 32'b0;
-				reg_array[8] <= 32'b0;
-				reg_array[9] <= 32'b0;
-				reg_array[10] <= 32'b0;
-				reg_array[11] <= 32'b0;
-				reg_array[12] <= 32'b0;
-				reg_array[13] <= 32'b0;
-				reg_array[14] <= 32'b0;
-				reg_array[15] <= 32'b0;
-				reg_array[16] <= 32'b0;
-				reg_array[17] <= 32'b0;
-				reg_array[18] <= 32'b0;
-				reg_array[19] <= 32'b0;
-				reg_array[20] <= 32'b0;
-				reg_array[21] <= 32'b0;
-				reg_array[22] <= 32'b0;
-				reg_array[23] <= 32'b0;
-				reg_array[24] <= 32'b0;
-				reg_array[25] <= 32'b0;
-				reg_array[26] <= 32'b0;
-				reg_array[27] <= 32'b0;
-				reg_array[28] <= 32'b0;
-				reg_array[29] <= 32'b0;
-				reg_array[30] <= 32'b0;
-				reg_array[31] <= 32'b0;
+                reg_array[0] <= 24'b0;  
+                reg_array[1] <= 24'b0;  
+                reg_array[2] <= 24'b0;  
+                reg_array[3] <= 24'b0;  
+                reg_array[4] <= 24'b0;  
+                reg_array[5] <= 24'b0;  
+                reg_array[6] <= 24'b0;  
+                reg_array[7] <= 24'b0;
+				reg_array[8] <= 24'b0;
+				reg_array[9] <= 24'b0;
+				reg_array[10] <= 24'b0;
+				reg_array[11] <= 24'b0;
+				reg_array[12] <= 24'b0;
+				reg_array[13] <= 24'b0;
+				reg_array[14] <= 24'b0;
+				reg_array[15] <= 24'b0;
 					 
            end  
            else begin  
@@ -56,6 +38,6 @@ module regfile
                 end  
            end  
       end  
-      assign reg_read_data_1 = ( reg_read_addr_1 == 0)? 32'b0 : reg_array[reg_read_addr_1]; // if reg == $zero -> return 0 
-      assign reg_read_data_2 = ( reg_read_addr_2 == 0)? 32'b0 : reg_array[reg_read_addr_2]; // if reg == $zero -> return 0 
+      assign reg_read_data_1 = ( reg_read_addr_1 == 0)? 24'b0 : reg_array[reg_read_addr_1]; // if reg == $zero -> return 0 
+      assign reg_read_data_2 = ( reg_read_addr_2 == 0)? 24'b0 : reg_array[reg_read_addr_2]; // if reg == $zero -> return 0 
  endmodule
