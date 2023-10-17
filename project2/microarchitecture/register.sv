b3102049ac4e11bc8f1b5ab9a32676f47b503761
LuisPeMoraRod <luismorarod98@estudiantec.cr>
2023-10-17 12:39:08 -0600

    feat(register): update register module size to 24 bits

--- project2/microarchitecture/register.sv
+++ project2/microarchitecture/register.sv
@@ -1,13 +1,12 @@
-//32 bit register 
-module register(RegOut,RegIn,WriteEn,reset,clk); 
-	output [31:0] RegOut;
-	input [31:0] RegIn;
+module register #(parameter N=24) (RegOut,RegIn,WriteEn,reset,clk); 
+	output [N-1:0] RegOut;
+	input [N-1:0] RegIn;
 	input WriteEn,reset, clk;
-	reg [31:0] q_reg;
+	reg [N-1:0] q_reg;
 	 
 	always @(posedge clk or posedge reset)
 		if (reset)
-			q_reg = 32'h00000000; // On reset, set to 0
+			q_reg = 24'h000000; // On reset, set to 0
 		else if (WriteEn)
 			q_reg = RegIn; // Otherwise RegOut = RegIn 
 
