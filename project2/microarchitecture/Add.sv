f7cb5c301b59067374609676060faf72635550ea
LuisPeMoraRod <luismorarod98@estudiantec.cr>
2023-10-17 12:53:03 -0600

    refactor(adder): update size of adder operands to 24 bits

--- project2/microarchitecture/Add.sv
+++ project2/microarchitecture/Add.sv
@@ -1,8 +1,8 @@
 `timescale 1 ps / 100 fs
 // Verilog code for 32-bit adder 
-module Add(S,A,B);
-    output [31:0] S;
-    input [31:0] A,B;
+module Add #(parameter N=24) (S,A,B);
+    output [N-1:0] S;
+    input [N-1:0] A,B;
 
     wire C, N, V, Z;
 
---
+++ project2/microarchitecture/adder.sv
@@ -0,0 +1,29 @@
+module adder #(parameter N=24)
+	 (input logic [N-1:0] A,
+	  input logic [N-1:0] B,
+	  input logic Cin,
+	  output logic [N-1:0] R,
+	  output C,
+	  output N,
+	  output V,
+	  output Z);
+	  
+	logic [N:0] cins, B_logic;
+	assign cins[0] = Cin;
+	assign B_logic = (Cin) ? ~B : B;
+
+	genvar i;
+	
+	generate
+		for (i = 0; i < N; i += 1) begin : GenAdders
+			full_adder U1 (.A(A[i]), .B(B_logic[i]), .cin(cins[i]), .cout(cins[i + 1]),
+									.R(R[i]));
+		end
+	endgenerate
+	
+	assign C = cins[N];
+	assign N = R[N-1];
+	assign Z = (R == 24'h000000) ? 1 : 0;
+	assign V = (~Cin && (A[N-1] == B[N-1]) && (R[N-1] != A[N-1])) || (Cin && (A[N-1] != B[N-1]) && (R[N-1] != A[N-1])); 
+
+endmodule
