e487207f7519437877e8864ced1bded59b998461
LuisPeMoraRod <luismorarod98@estudiantec.cr>
2023-10-17 13:46:10 -0600

    refactor(control): update control block to match with ISA's opcodes

--- project2/microarchitecture/Control.sv
+++ project2/microarchitecture/Control.sv
@@ -18,14 +18,18 @@ Funct
 
 output RegDst,ALUSrc,MemtoReg,RegWrite,MemRead,MemWrite,Branch,Jump,SignZero,BranchSrc;
 output [1:0] ALUOp;
-input [5:0] Opcode, Funct;
+input [2:0] Opcode; 
+input [3:0] Funct;
+
 reg RegDst,ALUSrc,MemtoReg,RegWrite,MemRead,MemWrite,Branch,Jump,SignZero,BranchSrc;
 reg [1:0] ALUOp;
+
 logic isShift;
-assign isShift = (Funct == 6'h00) || (Funct == 6'h02);
+assign isShift = (Funct == 4'h3) || (Funct == 4'h4);
+
 always @(*)
 casex (Opcode)
- 6'b000000 : begin // R - type
+ 3'b000 : begin // R - type
      RegDst = 1'b1;
      ALUSrc = (isShift) ? 1'b1 : 1'b0;
      MemtoReg= 1'b0;
@@ -38,59 +42,7 @@ casex (Opcode)
      Jump = 1'b0;
      SignZero= 1'b0;
     end
- 6'b100011 : begin // lw - load word
-     RegDst = 1'b0;
-     ALUSrc = 1'b1;
-     MemtoReg= 1'b1;
-     RegWrite= 1'b1;
-     MemRead = 1'b1;
-     MemWrite= 1'b0;
-     Branch = 1'b0;
-     BranchSrc = 1'b0;
-     ALUOp = 2'b00;
-     Jump = 1'b0;
-     SignZero= 1'b0; // sign extend
-    end
- 6'b101011 : begin // sw - store word
-     RegDst = 1'bx;
-     ALUSrc = 1'b1;
-     MemtoReg= 1'bx;
-     RegWrite= 1'b0;
-     MemRead = 1'b0;
-     MemWrite= 1'b1;
-     Branch = 1'b0;
-     BranchSrc = 1'b0;
-     ALUOp = 2'b00;
-     Jump = 1'b0;
-     SignZero= 1'b0;
-    end
- 6'b000100 : begin // beq - branch on equal
-     RegDst = 1'b0;
-     ALUSrc = 1'b0;
-     MemtoReg= 1'b0;
-     RegWrite= 1'b0;
-     MemRead = 1'b0;
-     MemWrite= 1'b0;
-     Branch = 1'b1;
-     BranchSrc = 1'b0;
-     ALUOp = 2'b01;
-     Jump = 1'b0;
-     SignZero= 1'b0; // sign extend
-    end
- 6'b000101 : begin // bgt - branch on greater than
-     RegDst = 1'b0;
-     ALUSrc = 1'b0;
-     MemtoReg= 1'b0;
-     RegWrite= 1'b0;
-     MemRead = 1'b0;
-     MemWrite= 1'b0;
-     Branch = 1'b1;
-     BranchSrc = 1'b1;
-     ALUOp = 2'b01;
-     Jump = 1'b0;
-     SignZero= 1'b0; // sign extend
-    end
- 6'b001110 : begin // xori - XOR immidiate
+ 3'b001 : begin // xori - XOR immidiate
      RegDst = 1'b0;
      ALUSrc = 1'b1;
      MemtoReg= 1'b0;
@@ -103,7 +55,59 @@ casex (Opcode)
      Jump = 1'b0;
      SignZero= 1'b0; // zero extend
     end
- 6'b000010 : begin // j - Jump
+ 3'b010 : begin // beq - branch on equal
+     RegDst = 1'b0;
+     ALUSrc = 1'b0;
+     MemtoReg= 1'b0;
+     RegWrite= 1'b0;
+     MemRead = 1'b0;
+     MemWrite= 1'b0;
+     Branch = 1'b1;
+     BranchSrc = 1'b0;
+     ALUOp = 2'b01;
+     Jump = 1'b0;
+     SignZero= 1'b0; // sign extend
+    end
+ 3'b011 : begin // bgt - branch on greater than
+     RegDst = 1'b0;
+     ALUSrc = 1'b0;
+     MemtoReg= 1'b0;
+     RegWrite= 1'b0;
+     MemRead = 1'b0;
+     MemWrite= 1'b0;
+     Branch = 1'b1;
+     BranchSrc = 1'b1;
+     ALUOp = 2'b01;
+     Jump = 1'b0;
+     SignZero= 1'b0; // sign extend
+    end
+ 3'b100 : begin // sw - store word
+     RegDst = 1'bx;
+     ALUSrc = 1'b1;
+     MemtoReg= 1'bx;
+     RegWrite= 1'b0;
+     MemRead = 1'b0;
+     MemWrite= 1'b1;
+     Branch = 1'b0;
+     BranchSrc = 1'b0;
+     ALUOp = 2'b00;
+     Jump = 1'b0;
+     SignZero= 1'b0;
+    end
+ 3'b101 : begin // lw - load word
+     RegDst = 1'b0;
+     ALUSrc = 1'b1;
+     MemtoReg= 1'b1;
+     RegWrite= 1'b1;
+     MemRead = 1'b1;
+     MemWrite= 1'b0;
+     Branch = 1'b0;
+     BranchSrc = 1'b0;
+     ALUOp = 2'b00;
+     Jump = 1'b0;
+     SignZero= 1'b0; // sign extend
+    end
+ 3'b110 : begin // j - Jump
      RegDst = 1'b0;
      ALUSrc = 1'b0;
      MemtoReg= 1'b0;
