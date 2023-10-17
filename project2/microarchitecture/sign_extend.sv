da1fff6234e238a102db8ef27d789e18a91f671e
LuisPeMoraRod <luismorarod98@estudiantec.cr>
2023-10-17 15:18:15 -0600

    refactor(zero extend): update zero extend output size

--- project2/microarchitecture/sign_extend.sv
+++ project2/microarchitecture/sign_extend.sv
@@ -3,5 +3,5 @@
 module sign_extend(sOut24,sIn13);
     output [23:0] sOut24;
     input [12:0] sIn13;
-    assign sOut24 = {{16{sIn13[12]}},sIn13};
+    assign sOut24 = {{11{sIn13[12]}},sIn13};
 endmodule
--- project2/microarchitecture/zero_extend.sv
+++ project2/microarchitecture/zero_extend.sv
@@ -2,8 +2,8 @@
 // fpga4student.com: FPGA projects, Verilog Projects, VHDL projects
 // Verilog project: 32-bit 5-stage Pipelined MIPS Processor in Verilog 
 // Zero-Extension
-module zero_extend(zOut32,zIn16);
-output [31:0] zOut32;
-input [15:0] zIn16;
-assign zOut32 = {{16{1'b0}},zIn16};
+module zero_extend(zOut24,zIn13);
+output [23:0] zOut24;
+input [12:0] zIn13;
+assign zOut24 = {{11{1'b0}},zIn13};
 endmodule
