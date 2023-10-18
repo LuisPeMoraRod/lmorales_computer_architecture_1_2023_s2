d599d2324d3d9e10bd117f040e5d119caa0700e6
LuisPeMoraRod <luismorarod98@estudiantec.cr>
2023-10-17 21:54:03 -0600

    refactor(ALU control): update ALU control signals to match ASIP ISA

--- project2/microarchitecture/ALUControl_Block.sv
+++ project2/microarchitecture/ALUControl_Block.sv
@@ -9,15 +9,15 @@ module ALUControl_Block( ALUControl, ALUOp, funct);
 	assign ALUControlIn = {ALUOp,funct};
 	always @(ALUControlIn)
 	casex (ALUControlIn)
-		8'b11xxxx: ALUControl=3'b001; //xor for xori
-		8'b00xxxx: ALUControl=3'b000; //add for sw-lw
-		8'b01xxxx: ALUControl=3'b010; //sub for beq
-		8'b100000: ALUControl=3'b000; //add for R-type add
-		8'b100001: ALUControl=3'b010; //sub for R-type sub
-		8'b100010: ALUControl=3'b110; //mult for R-type mult
-		8'b100011: ALUControl=3'b100; //shift left for R-type sll
-		8'b100100: ALUControl=3'b101; //shift right for R-type srl
-		8'b100101: ALUControl=3'b011; //less than for R-type slt
+		6'b11xxxx: ALUControl=3'b001; //xor for xori
+		6'b00xxxx: ALUControl=3'b000; //add for sw-lw
+		6'b01xxxx: ALUControl=3'b010; //sub for beq
+		6'b100000: ALUControl=3'b000; //add for R-type add
+		6'b100001: ALUControl=3'b010; //sub for R-type sub
+		6'b100010: ALUControl=3'b110; //mult for R-type mult
+		6'b100011: ALUControl=3'b100; //shift left for R-type sll
+		6'b100100: ALUControl=3'b101; //shift right for R-type srl
+		6'b100101: ALUControl=3'b011; //less than for R-type slt
 		default: ALUControl=3'b000;
 	endcase
 endmodule
